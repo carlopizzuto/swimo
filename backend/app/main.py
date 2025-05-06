bde31bcbeb809e60d490b607b2dcb34198f7906b
carlopizzuto <carlo.pizzutow@gmail.com>
2025-05-06 01:29:41 -0500

    fix/backend: added cors support

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api import movies, swipes
 
 app = FastAPI(title="Swimo API", version="0.0.1")
+
+# CORS configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(movies.router, prefix="/movies", tags=["movies"])
 app.include_router(swipes.router, prefix="/swipes", tags=["swipes"])
